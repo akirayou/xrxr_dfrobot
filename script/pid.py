9a6ea64cc7b496bfbb16c631750b38dafea6b2a2
akira noda <you.akira.noda@gmail.com>
2019-05-29 07:02:26 +0900

    bugfix

--- script/pid.py
+++ script/pid.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 KILL_TIMER=0.5
+PID_I_LIMIT=1.0
 from drv8830 import Motor
 def motor(l,r):
     motor.drv.drive(0,-l)
@@ -36,7 +37,9 @@ class PID:
         self.E=e=self.target-y
         d=(e-self._oldE)/dt
         self._i+=e*dt
-
+        if(1e-10<self.I):
+            i_limit=PID_I_LIMIT/self.I
+            self._i=max(min(self._i,i_limit), -i_limit)
         self.out=self.P*e + self.I*self._i +self.D*d
         self._oldE=e
         return self.out
@@ -59,9 +62,11 @@ theV = None
 theR = None
 motorL=None
 motorR=None
-
+odomDelay=None
 def have_odom():
-    return (rospy.get_rostime()-last_odom_time).to_sec() < KILL_TIMER
+    global odomDelay
+    odomDelay=(rospy.get_rostime()-last_odom_time).to_sec()
+    return odomDelay < KILL_TIMER
 def xyz_to_np(i):
     return np.array([i.x,i.y,i.z])
 def sub_odom(msg):
@@ -122,7 +127,7 @@ def wait_odom():
 if __name__ == "__main__":
     rospy.init_node('xrxr_pid')
     cfg = dynamic_reconfigure.client.Client("xrxr_dfrobot_cfg", timeout=6, config_callback=cfg_callback)
-    RATE=rospy.Rate(4)
+    RATE=rospy.Rate(2)
     odom = rospy.get_param('~odom', '/camera/odom/sample')
     rospy.loginfo("subscribe : %s",odom)
     twist = rospy.get_param('~cmd_vel', '/cmd_vel')
@@ -135,7 +140,9 @@ if __name__ == "__main__":
         rospy.loginfo( "Vx:%03f - %03f \t Rz:%03f - %03f \t Motor:%03f , %03f  ",
             PID_v.target, PID_v.E ,PID_r.target, PID_r.E,motorL,motorR)
         if(not have_odom()):
-            rospy.logerr("No odometry stop")
+            #motor()function have big delay on Error state. (such as no power)
+            #In case of that, subscriber loop (sub_odom) will not run correctory
+            rospy.logerr("No odometry stop %f or motor driver error",odomDelay)
             PID_v.stop()
             PID_r.stop()
             motor(0,0)
